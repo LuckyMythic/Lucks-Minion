d125a18a3849dafeda6db15cc37d3940f4d77c8a
LuckyMythic <70488800+LuckyMythic@users.noreply.github.com>
2020-09-10 09:59:39 -0500

    Update help.js

--- commands/info/help.js
+++ commands/info/help.js
@@ -9,7 +9,7 @@ exports.run = (client, message, args) => {
             .setColor("#FF4500")
             .setTitle("Commands")
             .addFields(
-                { name: 'Admin', value:"`addrole` `ban` `deletemsg` `kick` `mute` `removerole` `setbotname` `setbotstatus`"},
+                { name: 'Admin', value:"`addrole` `ban` `purge` `kick` `mute` `removerole` `setbotname` `setbotstatus`"},
                 { name: 'Info', value:"`help` `serverinfo` `serverinvite` `uptime` `userinfo`"},
                 { name: 'Music', value:"`deletetrack` `joinchannel` `nowplaying` `pause` `resume` `playother` `playtube` `queue` `skip` `stop` `volume` `ytsearch`"},
                 { name: 'Fun', value:"`distance` `dogs` `flipcoin` `google` `ping` `randomcolor` `rolldice` `setafk` `weather`"}
