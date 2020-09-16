7b00f4ad3c1f1db2f2d2991eed60f1f0f456ac6e
LuckyMythic <70488800+LuckyMythic@users.noreply.github.com>
2020-09-16 16:30:39 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,5 +3,5 @@ module.exports = (client) => {
 
 	client.user.setStatus('online');
 
-	client.user.setActivity('for vp.help', { type: 'LISTENING' });
+	client.user.setActivity('for vp.help', { type: 'WATCHING' });
 };
