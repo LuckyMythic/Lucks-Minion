098059d005a7f44b3ee67b339b0bf35744e9c66a
LuckyMythic <70488800+LuckyMythic@users.noreply.github.com>
2020-09-18 16:41:52 -0500

    Update checkadmin.js

--- commands/admin/checkadmin.js
+++ commands/admin/checkadmin.js
@@ -1,1 +1,14 @@
-//uwu
+exports.run = async (client, message, args) =>{
+  const deleteCount = parseInt(args[0], 10);
+
+  // get the delete count, as an actual number.
+  if(!message.member.hasPermission("MANAGE_MESSAGES")){
+    message.channel.send("You do not have perms to manage messages.");
+  }
+  
+  else{        
+    // Ooooh nice, combined conditions. <3
+
+    message.channel.send("You have perms to manage messages.");
+    }
+    
