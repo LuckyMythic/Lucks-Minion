ce1e6dac2c5329b15a02736bb8618d249d6da598
LuckyMythic <70488800+LuckyMythic@users.noreply.github.com>
2020-10-17 18:16:36 -0500

    Update testmana.js

--- commands/admin/testmana.js
+++ commands/admin/testmana.js
@@ -4,13 +4,13 @@ exports.run = async (bot, message, args) => {
   if(!message.member.hasPermission("KICK_MEMBERS")){
   if (mana = false) {
     mana = true;
-  }
-  else{
+  };
+  if (mana = true) {
     mana = false;
-  }
- }
+  };
+ };
 client.on('message', message => {
         if (mana = true) {
     message.channel.send("+<:Mana:766853530312376340>");
-    }
+    };
 });
