1cf7d391fe764aae46e7bc2d9fa5e53c205f4cfe
LuckyMythic <70488800+LuckyMythic@users.noreply.github.com>
2020-10-17 17:51:03 -0500

    Create testmana.js

---
+++ commands/admin/testmana.js
@@ -0,0 +1,16 @@
+var mana = require('mana');
+
+exports.run async (bot, message, args) => {
+  if(!message.member.hasPermission("KICK_MEMBERS")){
+  if mana = false{
+    mana = true;
+  }
+  else{
+    mana = false;
+  }
+ }
+client.on('message', message => {
+        if mana true{
+    message.channel.send("+<:Mana:766853530312376340>")
+    }
+});
