e7e434003a1427dbe1044129a7650da0c0fa191a
LuckyMythic <70488800+LuckyMythic@users.noreply.github.com>
2020-10-18 10:28:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -78,17 +78,13 @@ if (message.content == '-Manaoff') {
 
       message.react('766853530312376340')
       mana = true
-   else {
-    message.reply('You do not have the permission to run this command')
-  }
+
 }
-if (message.content == '-Manaoff') {
+else (message.content == '-Manaoff') {
 
       message.react('766853530312376340')
       mana = false
-   else {
-    message.reply('You do not have the permission to run this command')
-  }
+
 }
 
 client.on('message', message => {
