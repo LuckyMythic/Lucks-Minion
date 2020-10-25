6ebd46e9e8b88e1f333c985e00d91e92395bc766
LuckyMythic <70488800+LuckyMythic@users.noreply.github.com>
2020-10-24 20:58:40 -0500

    Update endtask.js

--- commands/admin/endtask.js
+++ commands/admin/endtask.js
@@ -1,10 +1,14 @@
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
-
+	if(!message.member.hasPermission("KICK_MEMBERS")){
+        return message.channel.send("You don't have the permissions to use this command!");
+    }
+	else{
 	message.reply('Bot has been stopped (or restarted if you use a process manager)')
 		.then(() => {
 			console.log(`Bot stopped by ${message.author.tag} (${message.author.id})`);
 			process.exit(0);
 		});
+	}
 };
 
 exports.conf = {
