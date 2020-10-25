34ba3e6eac8881e39c581419e01c1e346f36150f
LuckyMythic <70488800+LuckyMythic@users.noreply.github.com>
2020-10-24 21:03:16 -0500

    Update pin.js

--- commands/admin/pin.js
+++ commands/admin/pin.js
@@ -1,4 +1,8 @@
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
+		if(!message.member.hasPermission("KICK_MEMBERS")){
+        return message.channel.send("You don't have the permissions to use this command!");
+    }
+	else {
 	var id = args[0];
 	//if (!id) return message.reply('You need to give an ID of the message to pin');
 	if (!id) await message.channel.fetchMessages({ limit: 2 }).then((m) => id = m.array()[1].id);
@@ -13,7 +17,7 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
 		client.log('INFO', `Failed to pin message with ID of ${id} was pinned in ${message.channel.name} (${message.channel.id}) on guild ${message.guild.name} (${message.guild.id})`);
 	});
 };
-
+}
 exports.conf = {
 	enabled: true,
 	guildOnly: true,
